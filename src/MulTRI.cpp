7e04ab382054fcf2635a5651cf978335857e54b0
TalentlessAlpaca <milo.suarez@outlook.com>
2016-08-22 01:20:39 -0500

    Roxygen Section in MulTRI.R, starting documentation, removed user acces to c++ function.

--- src/MulTRI.cpp
+++ src/MulTRI.cpp
@@ -10,14 +10,14 @@ using namespace Rcpp;
 //[[Rcpp::plugins(cpp11)]]
 //[[Rcpp::plugins(openmp)]]
 
-//' Estimates the parameters of a test
-//'
-//' @param RData Input dataset.
-//' @param dim Model Dimension
-//' @param model 1 2 or 3PL
-//' @param EMepsilon Convergence value for the algorithm
-//' @param theta Quadrature Points
-//' @param weights Quadrature Points Weights
+// Estimates the parameters of a test
+//
+// @param RData Input dataset.
+// @param dim Model Dimension
+// @param model 1 2 or 3PL
+// @param EMepsilon Convergence value for the algorithm
+// @param theta Quadrature Points
+// @param weights Quadrature Points Weights
 // [[Rcpp::export]]
 List dichotomous ( IntegerMatrix RData, unsigned int dim, int model, double EMepsilon,
                    NumericMatrix theta, NumericVector weights ) {
